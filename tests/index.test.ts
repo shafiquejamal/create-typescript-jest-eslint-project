7a1bcfe33f54eaed41721217808ea0179f911fcb
shafiquejamal <shafique.jamal@gmail.com>
2025-03-15 12:30:24 -0400

    Add github action for test coverage

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,14 @@
-import { CONSTANT } from '../src';
+import { CONSTANT, exampleForTestCoverage } from '../src';
 
 describe('sample test', () => {
-	it('should pass', () => {
+	it('Should have a variable `CONSTANT` that is equal to the number 4', () => {
 		expect(CONSTANT).toEqual(4);
+	});
+
+	// See https://github.com/shafiquejamal/github-jest-coverage
+	it('Should partially cover the exampleForTestCoverage function :-p', () => {
+		expect(exampleForTestCoverage(6)).toEqual(
+			'Input number 6 was greater than 5',
+		);
 	});
 });
